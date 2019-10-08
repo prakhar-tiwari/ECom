fc77216faf7893cf56726f2700e7f19ae5155186
prak281993 <prakhartew@gmail.com>
2019-10-08 14:22:50 +0530

    Update keys.js

--- utils/keys.js
+++ utils/keys.js
@@ -1,6 +1,6 @@
 module.exports={
-    MONGODB_URI:"mongodb+srv://prakhar:admin@cluster0-qejpw.mongodb.net/superhero",
-    secretOrKey:'secret',
-    googleClientId:'382588452484-8ruhu1nhsl6tv73m4uft5rpp5929dkhl.apps.googleusercontent.com',
-    googleClientSecret:'xtHjU82lPHr848ahjNxHN2pr'
+    MONGODB_URI:"",
+    secretOrKey:'',
+    googleClientId:'',
+    googleClientSecret:''
 }
