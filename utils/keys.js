3c3d8bcf8a7352704705033d262b907b04ecbfe3
prak281993 <prakhartew@gmail.com>
2019-10-13 01:56:55 +0530

    Update keys.js

--- utils/keys.js
+++ utils/keys.js
@@ -1,8 +1,8 @@
-module.exports={
-    MONGODB_URI:"mongodb+srv://prakhar:admin@cluster0-qejpw.mongodb.net/superhero",
-    secretOrKey:'secret',
-    googleClientId:'382588452484-8ruhu1nhsl6tv73m4uft5rpp5929dkhl.apps.googleusercontent.com',
-    googleClientSecret:'xtHjU82lPHr848ahjNxHN2pr',
-    facebookAppId:'431728824393738',
-    facebookAppSecret:'a22de98a5eaf3fc306563d76fb8dae44'
+module.exports = {
+    MONGODB_URI: process.env.MONGODB_URI,
+    secretOrKey: process.env.secretOrKey,
+    googleClientId: process.env.googleClientId,
+    googleClientSecret: process.env.googleClientSecret,
+    facebookAppId: process.env.facebookAppId,
+    facebookAppSecret: process.env.facebookAppSecret
 }
